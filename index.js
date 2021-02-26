a08c8234ba5a5b2669fb7743dfb19c1d3c6a5140
Renato Santos <renato_jfs@hotmail.com>
2021-02-26 00:07:41 -0300

    Comentários, Strings e Numbers

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const nome = 'Renato'
-console.log(nome)
+// Criar um programa que calcula a media
+// das notas entre os alunos e envia
+// mensagem do calculo da media
+
+const aluno01 = "Renato"
+const aluno02 = 'Mariah'
+const notaAluno01 = 9.8
+const notaAluno02 = 10
