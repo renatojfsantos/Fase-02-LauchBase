da97c75923ec4ee9f771303c115a64ea050fec25
Renato Santos <renato_jfs@hotmail.com>
2021-02-26 00:13:13 -0300

    Fazendo cálculos com JavaScript

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// Criar um programa que calcula a media
+// Criar um programa que calcula a média
 // das notas entre os alunos e envia
-// mensagem do calculo da media
+// mensagem do cálculo da média
 
-const aluno01 = "Renato"
+const aluno01 = 'Renato'
 const aluno02 = 'Mariah'
+const aluno03 = 'Ana Liz'
 const notaAluno01 = 9.8
 const notaAluno02 = 10
+const notaAluno03 = 2
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+
+console.log(media)
