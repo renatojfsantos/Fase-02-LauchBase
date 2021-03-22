6bb4b716612086b53037144ba66e79f06e596b0a
Renato Santos <renato_jfs@hotmail.com>
2021-03-21 22:04:49 -0300

    Condicionais

--- index.js
+++ index.js
@@ -3,12 +3,18 @@
 // mensagem do cálculo da média
 
 const aluno01 = 'Renato'
-const aluno02 = 'Mariah'
-const aluno03 = 'Ana Liz'
 const notaAluno01 = 9.8
+
+const aluno02 = 'Mariah'
 const notaAluno02 = 10
+
+const aluno03 = 'Ana Liz'
 const notaAluno03 = 2
 
 const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
 
-console.log(media)
+if (media > 5) {
+  console.log(`A média foi de ${media}. Parabéns`)
+} else {
+  console.log('A média é menor que 5')
+}
